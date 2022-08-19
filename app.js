bd714381aa02d8c692194b8e65500be03ee6768a
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-08-19 20:57:36 +0200

    Added important comments about Promise usage.

--- app.js
+++ app.js
@@ -92,21 +92,27 @@ class TripService
 
     findByName(tripName)
     {
+        // Promises accept a work function, which is given an argument list from whoever calls Promise.then().
+        // This argument list is composed of success/fail handler functions.
+        // The error handling function ('reject' here) will *not* automagically short circuit the execution flow and jump into Promise.catch().
+        // To avoid bugs in the case of then() chains that run when they should not, the error handling function pointer itself has to propagate the error by throwing something for Promise to catch.
+        // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#chained_promises
         return new Promise((resolve, reject) =>
         {
             setTimeout(() =>
             {
-                for (const trip of this.trips.entries())
+                for (const trip of this.trips)
                 {
-                    if (trip[1].name == tripName)
-                        return resolve(trip[1]);
+                    if (trip.name == tripName)
+                        return resolve(trip);
                 }
-                
+
                 return reject(`No trip with name ${tripName}`);
             }, 2000)
         });
     }
 }
+
 class PriceService
 {
     constructor()
@@ -115,6 +121,7 @@ class PriceService
         this.trips.set('paris', 100);
         this.trips.set('rio-de-janeiro', 800);
     }
+
     findPriceByTripId(tripId)
     {
         return new Promise((resolve, reject) =>
@@ -135,7 +142,22 @@ class PriceService
 let tripService = new TripService();
 let priceService = new PriceService();
 
-tripService.findByName('Paris').then(t => console.log('Trip found : ' + t));
-tripService.findByName('Toulouse').catch(r => console.log(r));
-tripService.findByName('Rio de Janeiro').then(t => priceService.findPriceByTripId(t.id).then(p => console.log('Price found : ' + p)));
-tripService.findByName('Nantes').then(t => priceService.findPriceByTripId(t.id).then(p => console.log('Price found : ' + p))).catch(r => console.log(r));
+tripService.findByName('Paris').
+    then(t => console.log('Trip found : ' + t));
+
+tripService.findByName('Toulouse').
+    catch(r => console.log(r));
+
+tripService.findByName('Rio de Janeiro').
+    then(t => priceService.findPriceByTripId(t.id).
+        then(p => console.log('Price found : ' + p)));
+
+tripService.findByName('Nantes').
+    then(t => priceService.findPriceByTripId(t.id).
+        then(p => console.log('Price found : ' + p)),
+        e =>
+        {
+            // As noted above, if you wanted to specifically handle an error for this promise, you'd do it here, and absolutely throw something so that execution stops from here and jumps straight to Promise.catch(), if called below.
+            throw e;
+        }).
+    catch(r => console.log(r));
